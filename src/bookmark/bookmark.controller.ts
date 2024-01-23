60a56ae844cb034ada916fea277c75a9d69b4ef5
大森優太 <lostforest0@gmail.com>
2024-01-24 00:34:50 +0900

    fix: bookmarkの大枠を作成

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,58 @@
+import { JwtGuard } from '@/auth/guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from '@/auth/decorator';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+
+@UseGuards(JwtGuard)
+@Controller('bookmark')
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+  @Get()
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmarks(userId);
+  }
+
+  @Get(':id')
+  getBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
+
+  @Post()
+  createBookmark(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateBookmarkDto,
+  ) {
+    this.bookmarkService.createBookmark(userId, dto);
+  }
+
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {
+    this.bookmarkService.editBookmarkById(userId, bookmarkId, dto);
+  }
+
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    this.bookmarkService.deleteBookmarkById(userId, bookmarkId);
+  }
+}
---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+
+@Injectable()
+export class BookmarkService {
+  getBookmarks(userId: number) {
+    return;
+  }
+
+  getBookmarkById(userId: number, bookmarkId: number) {}
+
+  createBookmark(userId: number, dto: CreateBookmarkDto) {}
+
+  editBookmarkById(userId: number, bookmarkId: number, dto: EditBookmarkDto) {}
+
+  deleteBookmarkById(userId: number, bookmarkId: number) {}
+}
